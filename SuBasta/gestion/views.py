7eeb1f5dd823fba444f78715cc79b50207145e50
Erick Brandon <erick.brandon.flores@gmail.com>
2020-05-26 18:02:21 -0500

    registro views y html

--- SuBasta/gestion/views.py
+++ SuBasta/gestion/views.py
@@ -4,4 +4,13 @@ def hola(request):
     return render(request,"index.html")
 
 def Entrada(request):
-    return render(request,"formularios/Entrada.html")
+    contDia=1
+    rangoDia=[]
+    rangoAño=[]
+
+
+    mes=["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
+    for k in range(31):
+        rangoDia.append(k+1)
+    
+    return render(request,"formularios/Entrada.html",{"contDia":contDia,"rangoDia":rangoDia,"ListMes":mes})
