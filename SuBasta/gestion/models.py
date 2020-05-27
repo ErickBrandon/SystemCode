182b6df96d145125b9d380cc205eee135b1d4165
LeonardoMenOli <leo.moe@hotmail.com>
2020-05-26 23:14:51 -0500

    base de datos

--- SuBasta/gestion/models.py
+++ SuBasta/gestion/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Registro(models.Model):
+    nombre=models.CharField(max_length=50)
+    apellido=models.CharField(max_length=50)
+    email=models.EmailField()
+    contraseña=models.CharField(max_length=30)
