c1284f316d0a9dc927a6a39c05e41e5366273848
Erick Brandon <erick.brandon.flores@gmail.com>
2020-05-27 00:32:46 -0500

    cbx entrada

--- SuBasta/gestion/views.py
+++ SuBasta/gestion/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
+import datetime
 # Create your views here.
 def hola(request):
     return render(request,"index.html")
 
 def Entrada(request):
+    año_actual=datetime.datetime.now()
+    año=año_actual.year
     contDia=1
     rangoDia=[]
-    rangoAño=[]
+    ListAño=[]
+    for i in range(1920,año+1):
+        ListAño.append(i)
 
+    mes=["Enero","Febrero","Marzo",
+    "Abril","Mayo","Junio",
+    "Julio","Agosto","Septiembre",
+    "Octubre","Noviembre","Diciembre"]
 
-    mes=["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
     for k in range(31):
         rangoDia.append(k+1)
     
-    return render(request,"formularios/Entrada.html",{"contDia":contDia,"rangoDia":rangoDia,"ListMes":mes})
+    return render(request,"formularios/Entrada.html",
+    {"contDia":contDia,"rangoDia":rangoDia,
+    "ListMes":mes,
+    "ListAño":ListAño,})
