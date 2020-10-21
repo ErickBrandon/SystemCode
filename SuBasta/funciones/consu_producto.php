ee46a5bd0db8fc04d3b16c2c2e9a3147540104fa
LeonardoMenOli <leo.moe@hotmail.com>
2020-10-20 19:26:09 -0500

    SubasInv

--- SuBasta/funciones/consu_producto.php
+++ SuBasta/funciones/consu_producto.php
@@ -6,6 +6,7 @@
 
     $categoria = $mostrar['categoria'];
     $clave = $mostrar['clave'];
+    $precio = $mostrar['nuePrecio'];
 
     if ($categoria == "Tecnología") {
     	$conCat = "SELECT * FROM tecnologia WHERE ClaveP='$clave'";
@@ -13,5 +14,12 @@
     	$mostrarTec = mysqli_fetch_array($resCat);
     	mysqli_free_result($resCat);
     }
+
+    if(isset($_POST['nuePre'])) {
+    	$nuePre = $precio - 4;
+
+		$query="UPDATE producto SET nuePre='$nuePre' WHERE Id='$IdProd'";
+    
+}
     mysqli_close($conexion);
  ?>
