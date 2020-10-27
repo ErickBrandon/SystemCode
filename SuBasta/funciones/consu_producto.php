32865e92ab117fc235c390f6f69f578cdd8c9502
LeonardoMenOli <leo.moe@hotmail.com>
2020-10-26 18:26:24 -0600

    Inversa

--- SuBasta/funciones/consu_producto.php
+++ SuBasta/funciones/consu_producto.php
@@ -1,12 +1,12 @@
 <?php 
     $dat_p="SELECT * FROM producto WHERE Id='$IdProd'";
     $p=mysqli_query($conexion,$dat_p);
-    $mostrar=mysqli_fetch_array($p);
+    $mostrarP=mysqli_fetch_array($p);
     mysqli_free_result($p);
 
-    $categoria = $mostrar['categoria'];
-    $clave = $mostrar['clave'];
-    $precio = $mostrar['nuePrecio'];
+    $categoria = $mostrarP['categoria'];
+    $clave = $mostrarP['clave'];
+    $precio = $mostrarP['nuePrecio'];
 
     if ($categoria == "Tecnología") {
     	$conCat = "SELECT * FROM tecnologia WHERE ClaveP='$clave'";
@@ -16,9 +16,27 @@
     }
 
     if(isset($_POST['nuePre'])) {
-    	$nuePre = $precio - 4;
+        session_start();
+        $usuario=$_SESSION['usuario'];
+        $dat_u="SELECT * FROM us WHERE correo='$usuario'";
+        $u=mysqli_query($conexion,$dat_u);
+        $mostrar=mysqli_fetch_array($u);
+        mysqli_free_result($u);
+        $monedas = $mostrar['monedas'];
+        if ($monedas > 0 ){
 
-		$query="UPDATE producto SET nuePre='$nuePre' WHERE Id='$IdProd'";
+            $IdUsu = $mostrar['id_usuario'];
+            $Actmonedas = $monedas - 1;
+            $queryUsu="UPDATE us SET monedas='$Actmonedas' WHERE id_usuario='$IdUsu'";
+            $resultado = mysqli_query($conexion, $queryUsu);
+
+            $nuePre = $precio - 4;
+            $query="UPDATE producto SET nuePrecio='$nuePre' WHERE Id='$IdProd'";
+            $resultado = mysqli_query($conexion, $query);
+        }else{
+
+        }
+    	
     
 }
     mysqli_close($conexion);
