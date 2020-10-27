bbc977b2be5a78f1b3b6d51e2391e2a48d9121cc
LeonardoMenOli <leo.moe@hotmail.com>
2020-10-26 19:10:33 -0600

    Inversa_Pagina

--- SuBasta/pag/producto-inversa.php
+++ SuBasta/pag/producto-inversa.php
@@ -2,9 +2,6 @@
 <html lang="en">
 <head>
     <?php
-    if(isset($_POST['nuePre'])) {
-      
-    }
     include('../frac/generales.php');
     include("../funciones/conexionMysql.php");
     $IdProd = $_GET["IdProd"];
@@ -18,7 +15,7 @@
     include('../frac/header.php'); ?>
 <div id="ruta">
     <div class="linea">Modalidad-subasta<span>></span>Categoria<span>></span>Marca<span>></span>Producto</div>
-    <div class="nombre"><?php echo $mostrar['nombreP']?></div>
+    <div class="nombre"><?php echo $mostrarP['nombreP']?></div>
 </div>
 <section id=contenido>
   <div id="cont-producto">
@@ -27,27 +24,41 @@
             <div class="info-p">
               <ul>
                 <li class="p-i">Precio inicial:</li>
-                <li class="precio">$ <?php echo $mostrar['precio']?></li>
-                <li class="info-c">Categoría: <?php echo $mostrar['categoria']?></li>
-                <li class="info-c">Marca: <?php echo $mostrar['marca']?></li>
-                <li class="info-c">Modelo: <?php echo $mostrarTec['Modelo']?></li>
-                <li class="info-c">Condición: <?php echo $mostrar['condicion']?></li>
-                <li class="info-c">Tamaño: <?php echo $mostrarTec['Tamaño']?></li>
+                <li class="precio">$ <?php echo $mostrarP['precio']?></li>
+                <li class="info-c">Categoría: <?php echo $mostrarP['categoria']?></li>
+                <li class="info-c">Marca: <?php echo $mostrarP['marca']?></li>
+                <li class="info-c">Condición: <?php echo $mostrarP['condicion']?></li>
+
                 <!--Ropa y accesorios-->
+                <?php if ($categoria == "Ropa y calzado") {?>
                 <li class="info-c">Para:</li><!--Tambien para accesorios y belleza-->
                 <li class="info-c">Tipo:</li>
                 <li class="info-c">Tallas:</li>
+                <?php } ?>
+
+                <!--Tambien para accesorios y belleza-->
+                <?php if ($categoria == "Accesorios y belleza") {?>
+                <li class="info-c">Para:</li>
+                <?php } ?>
+
                 <!--Tecnologia-->
-                <li class="info-c">Tamaño:</li>
-                <li class="info-c">modelo:</li>
+                <?php if ($categoria == "Tecnología") { ?>
+                <li class="info-c">Tamaño: <?php echo $mostrarTec['Tamaño']?></li>
+                <li class="info-c">Modelo: <?php echo $mostrarTec['Modelo']?></li>
+                <?php } ?>
 
                 <!--otro-->
-                <li class="info-c">Otri:ctegoria</li>
+                <?php if ($categoria == "Otro") { ?>
+                <li class="info-c">Otra categoria: </li>
                 <li class="info-c">Tamaño:</li>
+                <?php } ?>
               </ul>
               <div class="tradicional">
                 <header>Nuevo precio<span class="fas fa-donate"></span></header>
                 <p class="mejor-precio">$ ??????</p>
+                <?php if(isset($_POST['nuePre'])) { ?>
+                    <p class="mejor-precio">$ </p>
+                <?php } ?>
                 <form action="producto-inversa.php?IdProd=<?php echo $IdProd ?>" method="POST">
                 <button class="dnp" type="submit" name="nuePre">Descubrir nuevo precio</button>
                 <button class="cah">Comprar ahora</button>
