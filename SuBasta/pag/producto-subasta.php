572fff6deb98e514a8b735de6ca0fcf478d8ada3
CcarretoC <70232440+CcarretoC@users.noreply.github.com>
2020-10-30 16:19:48 -0600

    Subasta

--- SuBasta/pag/producto-subasta.php
+++ SuBasta/pag/producto-subasta.php
@@ -5,6 +5,7 @@
     $IdProd = $_GET["IdProd"];
     include("../funciones/conexionMysql.php");
     include("../funciones/consu_producto.php");
+    include("../funciones/refrescarP.php");
      ?>
     <link rel="stylesheet" href="../recur/css/producto.css">
 
@@ -15,7 +16,7 @@
     include('../frac/header.php'); ?>
 <div id="ruta">
     <div class="linea">Modalidad-subasta<span>></span>Categoria<span>></span>Marca<span>></span>Producto</div>
-    <div class="nombre"><?php echo $mostrar['nombreP']?></div>
+    <div class="nombre"><?php echo $mostrarP['nombreP']?></div>
 </div>
 <section id=contenido>
   <div id="cont-producto">
@@ -24,27 +25,35 @@
             <div class="info-p">
               <ul>
                 <li class="p-i">Precio inicial:</li>
-                <li class="precio">$ <?php echo $mostrar['precio']?></li>
-                <li class="info-c">Categoría: <?php echo $mostrar['categoria']?></li>
-                <li class="info-c">Marca: <?php echo $mostrar['marca']?></li>
-                <li class="info-c">Modelo: <?php echo $mostrar['Modelo']?></li>
-                <li class="info-c">Condición: <?php echo $mostrar['condicion']?></li>
-                <li class="info-c">Tamaño: <?php echo $mostrar['Tamaño']?></li>
+                <li class="precio">$ <?php echo $mostrarP['precio']?></li>
+                <li class="info-c">Categoría: <?php echo $mostrarP['categoria']?></li>
+                <li class="info-c">Marca: <?php echo $mostrarP['marca']?></li>
+                <li class="info-c">Condición: <?php echo $mostrarP['condicion']?></li>
+
                 <!--Ropa y accesorios-->
-                <li class="info-c">Para:</li><!--Tambien para accesorios y belleza-->
+                <?php if ($categoria == "Ropa y calzado") {?>
+                <li class="info-c">Para:</li>
                 <li class="info-c">Tipo:</li>
                 <li class="info-c">Tallas:</li>
+                <?php } ?> 
+                <!--Tambien para accesorios y belleza-->
+                <?php if ($categoria == "Accesorios y belleza") {?>
+                <li class="info-c">Para:</li>
+                <?php } ?>              
                 <!--Tecnologia-->
-                <li class="info-c">Tamaño:</li>
-                <li class="info-c">modelo:</li>
-
+                <?php if ($categoria == "Tecnología") {?>
+                <li class="info-c">Tamaño: <?php echo $mostrarTec['Tamaño']?></li>
+                <li class="info-c">modelo: <?php echo $mostrarTec['Modelo']?></li>
+                <?php } ?>
                 <!--otro-->
-                <li class="info-c">Otri:ctegoria</li>
-                <li class="info-c">Tamaño:</li>
+                <?php if ($categoria == "Otro") {?>
+                <li class="info-c">Otra categoria: </li>
+                <li class="info-c">Tamaño: </li>
+                <?php } ?>
               </ul>
               <div class="tradicional">
                 <header>Mejor oferta <span class="fas fa-gavel"></span></header>
-                <p class="mejor-precio">$ 10,100.00</p>
+                <p id="recargaP" class="mejor-precio">$ 10,100.00</p>
                 <form action="">
                   <span>$</span>
                   <select name="" id="">
@@ -68,3 +77,13 @@
 </section>
 </body>
 </html>
+<script type="text/javascript">
+  $(document).ready(function(){
+      setInterval(
+                  function(){
+                  $('#recargaP').load('refrescarP.php');
+        },2000
+      );
+  }
+  );
+</script>
